83ab029d3017a343aec247947c2fc61520293380
Hygor Luciano <hygorluciano0@gmail.com>
2023-09-04 14:36:26 -0300

    Initial commit

---
+++ src/test/java/com/hygorluciano/forumalura/ForumAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hygorluciano.forumalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumAluraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
